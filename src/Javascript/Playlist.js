1e86a6abbcaf8698c65a8ff7a391b6ca1cd3f8b0
RidderLancelot <81768197+RidderLancelot@users.noreply.github.com>
2025-03-05 21:33:32 +0100

    Laver kæmpe reform snart, men det dur med det her nu :)

--- src/Javascript/Playlist.js
+++ src/Javascript/Playlist.js
@@ -1,9 +1,19 @@
+import Track from "./Track";
+import TopTracks from "./Song";
+
+const type = false;
+
 function Playlist() {
   return (
-    <div>
+    <div className="Playlist">
       <h1>Playlist</h1>
+      <div className="Tracklist">
+        {Track("wish i loved you in the 90s", "Tate Mcrae", "Too Young to Be Sad", type)}
+        {TopTracks()}
+      </div>
     </div>
   );
 }
 
-export default Playlist;
+
+export default (Playlist);
--- src/Javascript/Tracklist.js
+++ src/Javascript/Tracklist.js
@@ -1,16 +1,19 @@
 import Track from "./Track";
 
+const type = true;
+
 function Tracklist() {
   return (
     <div className="Tracklist">
-      {Track("bad ones", "Tate Mcrae", "Too Young to Be Sad")}
-      {Track("r u ok", "Tate Mcrae", "Too Young to Be Sad")}
-      {Track("you broke me first", "Tate Mcrae", "Too Young to Be Sad")}
-      {Track("slower", "Tate Mcrae", "Too Young to Be Sad")}
-      {Track("rubberband", "Tate Mcrae", "Too Young to Be Sad")}
-      {Track("wish i loved you in the 90s", "Tate Mcrae", "Too Young to Be Sad")}
+      {Track("bad ones", "Tate Mcrae", "Too Young to Be Sad", type)}
+      {Track("r u ok", "Tate Mcrae", "Too Young to Be Sad", type)}
+      {Track("you broke me first", "Tate Mcrae", "Too Young to Be Sad", type)}
+      {Track("slower", "Tate Mcrae", "Too Young to Be Sad", type)}
+      {Track("rubberband", "Tate Mcrae", "Too Young to Be Sad", type)}
+      {Track("wish i loved you in the 90s", "Tate Mcrae", "Too Young to Be Sad", type)}
     </div>
   );
 }
+
 
 export default Tracklist;
